356191cce86c33562cd54b39d9d6b1117dbb77c7
MD Nayem <nayemtp@gmail.com>
2023-10-11 23:37:55 +0600

    portfolio designe

---
+++ portfolio/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio-Demo</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <nav>
+            <h2 class="nav-title">Ma <span class="text-primary">r</span>y</h2>
+            <ul>
+                <li>Portfolio</li>
+                <li>Blogs</li>
+                <li><button class="btn-primary">Hire</button></li>
+            </ul>
+        </nav>
+        <!-- Header  Banner  -->
+        <div class="banner">
+            <div>
+                <h3 class="banner-sub-title">Hi, I am </h3>
+                <h1 class="banner-title">Mary Hardy</h1>
+                <p class="banner-description">Shot what able cold new see hold.Friendly as an betrayed formerly he. Morning because as to society behaved moments.</p>
+                <button class="btn-primary">Download CV</button>
+            </div>
+            <img src="images/hardy.png" alt="Picture Not Found">
+        </div>
+    </header>
+    <main>
+        <section class="about">
+            <h3 class="section-title">About Me</h3>
+            <p class="section-description">I, m a designer & developer with a passion for web design. I enjoy developing simple, clean and slick websites that provide real value to the end user. Thousands of clients have procured exceptional resulfs while working with me. Delivering work within time and budget which meets clients requirements in our mata.</p>
+            <div class="about-info-container">
+                <div class="about-info">
+                    <p class="info-title">Name:</p>
+                    <p class="info-description">Mary Hardy </p>
+                </div>
+                <div class="about-info">
+                    <p class="info-title">Email:</p>
+                    <p class="info-description">info@gmail.com</p>
+                </div>
+                <div class="about-info">
+                    <p class="info-title">Date of birth:</p>
+                    <p class="info-description">11 November 1987</p>
+                </div>
+                <div class="about-info">
+                    <p class="info-title">From:</p>
+                    <p class="info-description">Los Angeles, USA </p>
+                </div>
+            </div>
+        </section>
+        <!-- what i do section -->
+        <section class="what-i-do">
+            <h3 class="section-title">What I do</h3>
+            <p class="section-description">I have more than 10 years experience building software for clients all over the world. Below is a quick overview of my main technical skill sets and technologies i use. Want to find out more about my experience? Check out my online resume and project portfolio.</p>
+            <!-- .skills-container>.skill*4>img+p.skill-title+p.skill-description>lorem15 -->
+            <div class="skils-container">
+                <div class="skill">
+                    <img src="" alt="">
+                    <p class="skill-title"></p>
+                    <p class="skill-discription">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, ipsum! Sit cupiditate sunt dolor laboriosam.</p>
+                </div>
+                <div class="skill">
+                    <img src="" alt="">
+                    <p class="skill-title"></p>
+                    <p class="skill-discription">Quidem ab error ut voluptas, possimus, optio accusamus repudiandae quia nobis quo minus soluta dicta.</p>
+                </div>
+                <div class="skill">
+                    <img src="" alt="">
+                    <p class="skill-title"></p>
+                    <p class="skill-discription">Ipsa, itaque amet perspiciatis quam maxime sed consectetur fuga dolorum numquam suscipit eos molestiae corporis.</p>
+                </div>
+                <div class="skill">
+                    <img src="" alt="">
+                    <p class="skill-title"></p>
+                    <p class="skill-discription">Quibusdam quo assumenda deserunt sint expedita possimus eos? Maxime veritatis culpa perspiciatis voluptates, molestias neque.</p>
+                </div>
+            </div>
+                <div class="skill">
+                    <img src="" alt="">
+                    <p class="skill-title"></p>
+                    <p class="skill-discription">Quibusdam quo assumenda deserunt sint expedita possimus eos? Maxime veritatis culpa perspiciatis voluptates, molestias neque.</p>
+                    <p class="skill-discription">Quibusdam quo assumenda deserunt sint expedita possimus eos? Maxime veritatis culpa perspiciatis voluptates, molestias neque.</p>
+                </div>
+            </div>
+
+
+
+        </section>
+        </section>
+    </main>
+
+</body>
+
+</html>
